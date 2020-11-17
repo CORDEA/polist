3bd43373467297dd2c6868e28631f1ccb0b361ca
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-17 10:10:00 +0900

    set scope

--- packages/poly_api/lib/src/poly_api_client.dart
+++ packages/poly_api/lib/src/poly_api_client.dart
@@ -2,7 +2,7 @@ import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sig
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/poly/v1.dart';
 
-const _scopes = [""];
+const _scopes = ["https://www.googleapis.com/auth/vrassetdata.readonly"];
 
 class PolyApiClient {
   GoogleSignIn _googleSignIn = GoogleSignIn.standard(
