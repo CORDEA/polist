36a6a36e562faa5e2a9840b6512a0360f56564d3
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-23 18:15:11 +0900

    navigate

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  static Route route() => MaterialPageRoute(builder: (_) => HomePage());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:polist/home_page.dart';
+import 'package:polist/login/login_bloc.dart';
 
 class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Log in'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {},
-          child: const Text('Log in'),
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccess) {
+          Navigator.of(context)
+              .pushAndRemoveUntil(HomePage.route(), (route) => false);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Log in'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {},
+            child: const Text('Log in'),
+          ),
         ),
       ),
     );
