958214229e44ff6b8c3fdeaef6d5c17e3d9d729b
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-23 18:12:32 +0900

    provide bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:polist/login/login_bloc.dart';
 import 'package:polist/login_page.dart';
 
 void main() {
@@ -22,7 +24,10 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LogInPage(),
+      home: BlocProvider(
+        create: (context) => LoginBloc(_authenticationRepository),
+        child: LogInPage(),
+      ),
     );
   }
 }
