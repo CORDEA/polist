a2a197f98fa1eea01faf26d55380b5adfa79154a
Yoshihiro Tanaka <contact@cordea.jp>
2020-12-12 16:14:38 +0900

    add detail page

---
+++ lib/detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  static Route route() => MaterialPageRoute(builder: (_) => DetailPage());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
