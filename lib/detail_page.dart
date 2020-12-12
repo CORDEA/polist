58228e4d1293bce91da4a188c3f90265c55fa0a8
Yoshihiro Tanaka <contact@cordea.jp>
2020-12-12 16:30:06 +0900

    pass asset

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,10 +1,24 @@
+import 'package:asset_repository/asset_repository.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  static Route route() => MaterialPageRoute(builder: (_) => DetailPage());
+  static Route route(Asset asset) =>
+      MaterialPageRoute(builder: (_) => DetailPage(asset: asset));
+
+  DetailPage({Key key, @required Asset asset})
+      : assert(asset != null),
+        _asset = asset,
+        super(key: key);
+
+  final Asset _asset;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_asset.displayName),
+      ),
+      body: Container(),
+    );
   }
 }
